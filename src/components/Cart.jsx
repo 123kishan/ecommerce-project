7cb38f6cc47df7138bef84ee28bae3187e21c328
123kishan <mishrakishan085@gmail.com>
2023-10-12 01:44:09 +0530

    adding cart

---
+++ src/components/Cart.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import "./cart.css"
+import CartContext from '../../Context/CartContext'
+
+function Cart(props) {
+  
+  const cartctx = React.useContext(CartContext);
+   const NumOfItems = cartctx.Items.length;
+  return (
+    <>
+    <Container>
+    <div onClick={()=>{props.onClick()}} className='Cart'>
+        <h2>Cart</h2>
+        <p>{NumOfItems}</p>
+    </div>
+    </Container>
+    </>
+  )
+}
+
+export default Cart;
---
+++ src/components/CartComponentData.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import CartItem from './CartItem'
+import CartContext from '../../Context/CartContext'
+function CartComponentData() {
+ 
+  const cartctx = React.useContext(CartContext);
+   console.log(cartctx, "int cart comoponent")
+  return (
+     <div>
+      <div className='Cart_Items_data'>
+
+        {cartctx.Items.map(item=>{
+          return <CartItem Name={item.Name} Img={item.Img} price={item.Price}  />
+        })}
+    </div>
+       <div className="Cart_total_price d-flex justify-content-end">
+         <p className="fs-3   ">Total $0</p> 
+      </div>
+</div>
+  )
+}
+
+export default CartComponentData;
